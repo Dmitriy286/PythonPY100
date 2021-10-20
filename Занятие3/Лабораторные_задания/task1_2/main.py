83d705c3c333b59c3a9704e8cf67024b5b50cc4a
Maroders <Maroderstyuiop@gmail.com>
2021-10-20 21:26:31 +0300

    init

--- Занятие3/Лабораторные_задания/task1_2/main.py
+++ Занятие3/Лабораторные_задания/task1_2/main.py
@@ -1,11 +1,11 @@
 def factorial (n):
-    current_number = 1
+    current_number = 0
     fact = 1
-    for value in range(0,n):
+    for value in range(1, (n+1)):
         current_number += 1
         fact *= current_number
 
     return fact
 
 if __name__ == "__main__":
-    print(factorial(7))
+    print(factorial(5))
