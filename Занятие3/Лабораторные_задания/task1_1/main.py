2f9618b757a317ec5c64ef374c4e5a38f377b681
Maroders <DShishlov@gmail.com>
2021-10-25 14:42:08 +0300

    Done

--- Занятие3/Лабораторные_задания/task1_1/main.py
+++ Занятие3/Лабораторные_задания/task1_1/main.py
@@ -1,3 +1,4 @@
+# Первый вариант:
 def task_1_1():
     max_sum = 500
     current_sum = 0
@@ -10,7 +11,7 @@ def task_1_1():
 
         if current_sum + current_value > max_sum:
             break
-            return  n
+
         else:
             current_sum += current_value
             print(n, current_sum)
@@ -22,8 +23,20 @@ def task_1_1():
             # s = max(list_)
             # print((s))
 
-    return n
+    return list_
 
 if __name__ == "__main__":
-    print(task_1_1())
+    a = task_1_1()
 
+    for i, global_current_value in enumerate(a, start=1):
+        if global_current_value == max(a):
+            print("Количество чисел составляет: ", i)
+
+# Второй вариант:
+def task_1_1():
+    max_sum = 500
+    current_sum = 0
+    n = 1
+
+if __name__ == "__main__":
+    a = task_1_1()
--- Занятие3/Практические_задания/task3_1/main.py
+++ Занятие3/Практические_задания/task3_1/main.py
@@ -1,11 +1,32 @@
-def remove_whitespace(str_):
-    list_words = str_.split()  # TODO с помощью методов строки join и split очистить строку от лишних пробелов
-    result = "_".join(list_words)
-    print(list_words, 3)
-    return result
+# def remove_whitespace(str_):
+#     list_words = str_.split()  # TODO с помощью методов строки join и split очистить строку от лишних пробелов
+#     result = "_".join(list_words)
+#     print(list_words, 3)
+#     return result
+#
+#
+# if __name__ == "__main__":
+#     str_with_space = "    123.    test   print test11    "  # исходная строка
+#     print(str_with_space, 1)
+#     print(remove_whitespace(str_with_space), 2)
+
+lict_=[]
+a = [1, 2, 4]
+b = 7
+# lict_.append(a)
+# b = list(a)
+# b = lict_ + a
+#
+# print(lict_)
+print(a)
 
 
-if __name__ == "__main__":
-    str_with_space = "    123.    test   print test11    "  # исходная строка
-    print(str_with_space, 1)
-    print(remove_whitespace(str_with_space), 2)
+print(b)
+
+c = a + b
+print(c)
+# print(b)
+
+# lict_ = lict_ + list(a)
+#
+# print(lict_)
