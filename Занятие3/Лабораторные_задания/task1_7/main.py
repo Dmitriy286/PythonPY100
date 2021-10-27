5de2f0878adc9ffe9122f68dd57551b179090f1c
Maroders <DShishlov@gmail.com>
2021-10-27 17:10:22 +0300

    Done

--- Занятие3/Лабораторные_задания/task1_7/main.py
+++ Занятие3/Лабораторные_задания/task1_7/main.py
@@ -1,3 +1,12 @@
+def poor_student(s, A, b, q = 1.05):
+    months = 0
+    current_sum = s
+    while current_sum > b:
+        current_sum = s + A
+        rest_sum = current_sum - b
+        b = b * q
+        months += 1
+    return months
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    print(poor_student(1000, 500, 600))
