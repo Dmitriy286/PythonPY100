2c9d930465aaaa29ef1ed0424f92a383a0242c9c
Maroders <DShishlov@gmail.com>
2021-11-02 21:52:56 +0300

    Changes, 02.11.2021

--- Занятие4/Лабораторные_задания/task2_2/main.py
+++ Занятие4/Лабораторные_задания/task2_2/main.py
@@ -1,3 +1,28 @@
+# def func():
+#     str_ = set(input("Введите двузначное число: "))
+#     number_1 = set("48")
+#     number_2 = set("9")
+#     print(number_1)
+#     print(number_2)
+#     for value in str_:
+#         if str_ == number_1:
+#             print("Входит 4, 8")
+#         elif value in number_2:
+#             print("Входит 9")
+#         else:
+#             print("Не входит")
+#
+# if __name__ == "__main__":
+#     func()
+
+def func(number):
+    mumber_str = str(number)
+    print("Входит") if ("4" in mumber_str and "8" in mumber_str) or ("9" in mumber_str) else print("Не входит")
+
+    # if ("4" in mumber_str and "8" in mumber_str) or ("9" in mumber_str):
+    #     print("Входит")
+    # else:
+    #     print("Не входит")
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    func(input("Введите двузначное число: "))
--- Занятие4/Лабораторные_задания/task2_3/main.py
+++ Занятие4/Лабораторные_задания/task2_3/main.py
@@ -1,3 +1,25 @@
+def func(number):
+    number_local = str(number)
+    # sum_ = number_local.split()
+    list_ = []
+    for i in number_local:
+        list_.append(int(i))
+    sum_ = sum(list_)
+    print(sum_)
+    return sum_
+
+def compare(sum_):
+    list_1 = []
+    for i, b in enumerate(str(sum_)):
+        list_1.append(int(b))
+    print(list_1)
+
+    if len(list_1) == 2:
+        print("Сумма цифр является двузначным числом")
+    else:
+        print("Сумма цифр не является двузначным числом")
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    sum_ = func(input("Введите трехзначное число: "))
+    compare(sum_)
+
